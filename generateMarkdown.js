e68155d44acfcb5e6af06bf08540cae608e7b304
Blu VIII <34254871+BluVolpe@users.noreply.github.com>
2021-04-23 13:30:06 -0400

    Add files via upload

---
+++ generateMarkdown.js
@@ -0,0 +1,91 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string\
+
+function renderLicenseBadge(license) {
+  switch (license) {
+    case "MIT": return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+      
+      break;
+    
+      case "APACHE 2.0": return "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
+    
+      break;
+
+      case "GPL 3.0": return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)"
+     
+      break;
+
+      case "BSD 3": return "[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)" 
+    
+      break;
+
+      case "None": return ""
+    
+      break;
+
+    default:
+      break;
+  }
+}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {
+
+  switch (license) {
+    case "MIT": return "(https://opensource.org/licenses/MIT)"
+      
+      break;
+  
+    default:
+      break;
+  }
+}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+
+  ${renderLicenseBadge(data.license) 
+    
+  }
+    
+  }}
+  
+## Description
+${data.description}
+## Table of Contents (Optional)
+If your README is long, add a table of contents to make it easy for users to find what they need.
+- [Installation](#installation)
+- [Usage](#usage)
+- [Questions](#questions)
+- [License](#license)
+- [Technologies](#tech)
+- [Image](#img)
+- [Contribution](#contribution)
+## Installation
+${data.install}
+## Usage
+${data.usage}
+## Questions
+https://github.com/${data.github}
+## License
+
+ ${renderLicenseLink(data.license)}
+
+## Tech
+${data.tech}
+## Img
+${data.img}
+If your project has a lot of features, list them here.
+## Contribution
+${data.contribution}
+## Tests
+${data.test}
+`}
+
+module.exports = generateMarkdown;
---
+++ index.js
@@ -0,0 +1,83 @@
+// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./generateMarkdown");
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: "input",
+        message: "What is your project's name?",
+        name: "title",
+      },
+      {
+        type: "input",
+        message: "What is your Github Username?",
+        name: "github",
+      },
+      {
+        type: "input",
+        message: "What is your e-mail address?",
+        name: "email",
+      },
+     
+      {
+        type: "input",
+        message: "Please write a short description of your project..",
+        name: "description",
+      },
+      {
+        type: "input",
+        message: "Please provide details on how to use this application.",
+        name: "usage",
+      },
+      {
+        type: "list",
+        message: "What kind of license should your project have? ",
+        name: "license",
+        choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"],
+      },
+      {
+        type: "input",
+        message: "What command should be run to install dependencies?",
+        name: "install",
+      },
+     
+      {
+        type: "input",
+        message: "List the technologies used in this project..",
+        name: "tech",
+      },
+      {
+        type: "input",
+        message: "Advise users on how they can contribute to this project",
+        name: "contribution",
+      },
+      {
+        type: "input",
+        message: "Would you like to add any images? (to input image, open an issue in github, drop screenshot into open issue and paste the link)",
+        name: "img",
+      },
+      {
+        type: "input",
+        message: "Describe the testing process for this app.",
+        name: "test",
+      },
+];
+
+
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, content) {
+    fs.writeFileSync(fileName, content);
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt(questions).then(function (data) {
+        let content = generateMarkdown(data);
+        writeToFile("README.md", content);
+    });
+}
+
+// Function call to initialize app
+init();
